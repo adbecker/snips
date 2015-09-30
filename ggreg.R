3eb9e0b0bd00309c849a1d7f7dea8540e5c92616
adbecker <ab4035@nyu.edu>
2015-09-29 23:06:29 -0400

    ggplot regression code

---
+++ ggreg.R
@@ -0,0 +1,18 @@
+
+require(ggplot2)
+
+fit <- lm(y ~ x)
+
+
+ggreg <- function(fit){
+  
+  rsquared <- signif(summary(fit)$adj.r.squared, 2)
+  pval <- signif(summary(fit)$coef[2,4], 2)
+  
+  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+    geom_point() +
+    stat_smooth(method = "lm", col = "black") +
+    ggtitle(bquote(Adjusted~' '~R^{2}==.(rsquared)~', '~pval==.(pval)))  
+  
+}
+
