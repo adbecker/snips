1904358edda56f95a1d3b84ff7bc3219b7005912
adbecker <ab4035@nyu.edu>
2015-10-01 20:51:36 -0400

    make a gif

---
+++ makeGIF.R
@@ -0,0 +1,12 @@
+saveGIF({
+  
+  for(it in 1:Nmax){
+    
+    print(p)
+    print(it/Nmax*100)
+    
+  }
+  
+  
+},  interval = 0.02, movie.name = "title.gif", ani.width = 700, ani.height = 400)
+
